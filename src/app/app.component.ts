6fbeef4de58ab62051384fdbdc7c15000064b249
mark953fps <markville953@gmail.com>
2021-09-01 15:15:23 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   dataCheckBox: any = [
-    { id: 1, name: 'checkbox1' },
-    { id: 2, name: 'checkbox2' },
-    { id: 3, name: 'checkbox3' },
-    { id: 4, name: 'checkbox4' },
-    { id: 5, name: 'checkbox5' },
-    { id: 6, name: 'checkbox6' },
-    { id: 7, name: 'checkbox7' }
+    { id: 1, name: 'checkbox1', isChecked: false },
+    { id: 2, name: 'checkbox2', isChecked: false },
+    { id: 3, name: 'checkbox3', isChecked: false },
+    { id: 4, name: 'checkbox4', isChecked: false },
+    { id: 5, name: 'checkbox5', isChecked: false },
+    { id: 6, name: 'checkbox6', isChecked: false },
+    { id: 7, name: 'checkbox7', isChecked: false }
   ];
 
   public ngOnInit() {}
+
+  public onSelectChange(event: any, i: any): void {
+    this.dataCheckBox[i].isChecked = event;
+
+    if (this.dataCheckBox[i].isChecked) {
+      this.dataCheckBox[i].name = this.dataCheckBox[i].name.split('').join(',');
+    } else {
+      if (this.dataCheckBox[i].name.split(',')) {
+        this.dataCheckBox[i].name = this.dataCheckBox[i].name
+          .split(',')
+          .join('');
+      }
+    }
+  }
+
+  public labelFormat(i: any): any {}
 }
